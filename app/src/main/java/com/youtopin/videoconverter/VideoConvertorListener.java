97724e341976bd9385cd30f19600b54450fa69fd
sepehr <sepehrgh01@gmail.com>
2020-06-15 18:08:04 +0430

    Removed VideoConvertorListener separate interface

--- app/src/main/java/com/youtopin/videoconverter/VideoConvertorListener.java
+++
@@ -1,6 +0,0 @@
-package com.youtopin.videoconverter;
-
-public interface VideoConvertorListener {
-    boolean checkConversionCanceled();
-    void didWriteData(long availableSize,float progress);
-}
